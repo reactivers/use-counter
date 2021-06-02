a8376d8432c5215c18348b38cc9c091ec8afe2d1
Murat Guney <muratguney1995@gmail.com>
2021-06-02 23:42:11 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+> # @reactivers/use-counter
+
+[![npm version](https://badge.fury.io/js/@reactivers%2Fuse-counter.svg)](//www.npmjs.com/package/@reactivers/use-counter)
+
+# Welcome
+
+## Introduction
+
+> ```@reactivers/use-counter``` simple counter hook in React.
+
+## Docs
+You can find detailed docs [here](https://hooks.reactivers.com/use-counter)
+
+## Getting Started
+
+### Installation
+
+```bash
+npm install --save @reactivers/use-counter
+
+yarn add @reactivers/use-counter
+```
---
+++ dist/index.cjs.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var react = require('react');
+
+var useCounter = function (params) {
+    if (params === void 0) { params = { initial: 0 }; }
+    var _a = react.useState(params.initial), counter = _a[0], setCounter = _a[1];
+    var increase = react.useCallback(function (by) {
+        if (by === void 0) { by = 1; }
+        setCounter(function (old) { return old + by; });
+    }, []);
+    var decrease = react.useCallback(function (by) {
+        if (by === void 0) { by = 1; }
+        setCounter(function (old) { return old - by; });
+    }, []);
+    var reset = react.useCallback(function () {
+        setCounter(params.initial);
+    }, [params.initial]);
+    return { counter: counter, setCounter: setCounter, reset: reset, increase: increase, decrease: decrease };
+};
+
+exports.useCounter = useCounter;
